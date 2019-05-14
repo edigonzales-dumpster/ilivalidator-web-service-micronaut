f27f6c42ef2eb009936485ec1e8557352350deb2
Stefan Ziegler <edi.gonzales@gmail.com>
2019-05-14 21:18:08 +0200

    initial native binary support (w/o ilivalidator)

--- src/main/java/ch/so/agi/ilivalidator/controllers/MainController.java
+++ src/main/java/ch/so/agi/ilivalidator/controllers/MainController.java
@@ -2,7 +2,18 @@ package ch.so.agi.ilivalidator.controllers;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.multipart.CompletedFileUpload;
+import io.micronaut.views.View;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,9 +22,36 @@ import org.slf4j.LoggerFactory;
 public class MainController {
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
+    private static String FOLDER_PREFIX = "ilivalidator_";
+
     @Get("/")
+    @View("upload")
     public HttpStatus index() {
         System.out.println("bar");
         return HttpStatus.OK;
     }
+    
+    @Post(value = "/", consumes = MediaType.MULTIPART_FORM_DATA, produces = MediaType.TEXT_PLAIN) 
+    @View("upload")
+    public HttpResponse<String> validate(CompletedFileUpload file) {
+        System.out.println("fubar");
+        try {
+            Path tmpDirectory = Files.createTempDirectory(Paths.get(System.getProperty("java.io.tmpdir")), FOLDER_PREFIX);
+            Path uploadFilePath = Paths.get(tmpDirectory.toString(), file.getFilename());
+
+            byte[] bytes = file.getBytes();
+            Files.write(uploadFilePath, bytes);
+            
+            log.info(uploadFilePath.toFile().getAbsolutePath());
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            return HttpResponse.badRequest("Something went wrong.\n\n" + e.getMessage());
+        }
+        
+        
+        return HttpResponse.ok("fubar");
+    }
+
 }
