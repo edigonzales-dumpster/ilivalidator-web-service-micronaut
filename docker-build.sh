54833c35a599732c2b7478543c592c154e6daaf8
Stefan Ziegler <edi.gonzales@gmail.com>
2019-05-18 18:01:54 +0200

    add a test and travis

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
-docker build . -t sogis/ilivalidator-web-service-micronaut
+
+githash=$1
+if [ "x$githash" = "x" ]; then
+    githash='localbuild'
+fi
+
+buildident=$2
+if [ "x$buildident" = "x" ]; then
+    buildident='localbuild'
+fi
+
+build_timestamp=$(date '+%Y-%m-%d_%H:%M:%S')
+
+docker build . -t sogis/ilivalidator-web-service-micronaut:$buildident \
+    --label gretl.created=$build_timestamp --label gretl.git_commit=$githash --label gretl.travis_build=$buildident
+
+docker tag sogis/ilivalidator-web-service-micronaut:$buildident sogis/ilivalidator-web-service-micronaut:latest    
 echo
 echo
 echo "To run the docker container execute:"
--- src/test/java/ch/so/agi/MainControllerTest.java
+++ src/test/java/ch/so/agi/MainControllerTest.java
@@ -1,8 +1,11 @@
 package ch.so.agi;
 
 import io.micronaut.context.ApplicationContext;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.multipart.MultipartBody;
 import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.test.annotation.MicronautTest;
 
@@ -11,6 +14,11 @@ import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @MicronautTest
 public class MainControllerTest {
@@ -21,7 +29,24 @@ public class MainControllerTest {
     @Test
     public void testIndex() throws Exception {
         try(RxHttpClient client = embeddedServer.getApplicationContext().createBean(RxHttpClient.class, embeddedServer.getURL())) {
-            assertEquals(HttpStatus.OK, client.toBlocking().exchange("/main").status());
+            assertEquals(HttpStatus.OK, client.toBlocking().exchange("/ilivalidator").status());
+        }
+    }
+    
+    @Test
+    public void validation_Ok() throws Exception {
+        try(RxHttpClient client = embeddedServer.getApplicationContext().createBean(RxHttpClient.class, embeddedServer.getURL())) {
+
+            final MultipartBody body = MultipartBody.builder()
+                    .addPart("file", new File("src/test/data/254900.itf"))
+                    .build();
+            
+            String response = client.toBlocking().exchange(
+                    HttpRequest.POST("/ilivalidator", body)
+                            .contentType(MediaType.MULTIPART_FORM_DATA_TYPE), String.class)
+                    .body();
+            
+            assertTrue(response.contains("Info: ...validation done"));
         }
     }
 }
